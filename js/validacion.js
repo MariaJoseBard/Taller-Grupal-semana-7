787537c6eba5e3d58ba5805704209bbccb11ce91
MariaJoseBard <bardesio.mariajose@gmail.com>
2024-08-15 21:01:32 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,29 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function() {  
+     const nombre = document.getElementById("nombre")
+     const apellido = document.getElementById("apellido")
+     const email = document.getElementById("email")
+     const password = document.getElementById("password1")
+     const passwordDos = document.getElementById("password2")
+     const checkbox = document.getElementById("terminos")
+
+
+
+
+
+    if ((nombre.value === "" )|| (apellido.value === "") || (email.value === "") || (password.value.length < 6) || (password.value !== passwordDos.value) || (!checkbox.checked )) {
+        showAlertError();
+      
+    }
+        else { showAlertSuccess();
+
+    }
+    } 
+ );
+    
+
+
+
